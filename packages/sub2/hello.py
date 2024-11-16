21e3839b402ad7ea643b19499e1d8f44d30b7a5d
tf63 <74246282+tf63@users.noreply.github.com>
2024-11-16 20:52:03 +0900

    feat: setup subpackage

--- packages/sub2/hello.py
+++ packages/sub2/hello.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from example!")
+    print("Hello from sub2!")
 
 
 if __name__ == "__main__":
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from packages.sub.src.sub.hello import hello
+
+# from sub.hello import hello
+
+hello()
