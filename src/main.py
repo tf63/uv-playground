5cfd983e5da4672177892f85f52b52a3fca030d0
tf63 <74246282+tf63@users.noreply.github.com>
2024-11-16 21:39:42 +0900

    feat: configure package

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from packages.sub.src.sub.calc import calc_vector
-from packages.sub.src.sub.hello import hello
+from packages.sub.src.calc import calc_vector
+from packages.sub.src.hello import hello
 from packages.sub2.hello import hello as hello2
 
 hello()
